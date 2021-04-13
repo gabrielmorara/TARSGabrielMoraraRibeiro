abfa15b050d0eb8b81c633b00063c1e76b65976a
Gabriel Morara <gabriel.morara@gmail.com>
2021-04-13 20:45:19 -0300

    implementa edicao de produto

--- TARSGabrielMoraraRibeiro/Controllers/ProductsController.cs
+++ TARSGabrielMoraraRibeiro/Controllers/ProductsController.cs
@@ -21,6 +21,11 @@ namespace TARSGabrielMoraraRibeiro.Controllers
             return ValidateUser();
         }
 
+        public IActionResult EditProduct()
+        {
+            return ValidateUser();
+        }
+
         public IActionResult ListProducts()
         {
             return ValidateUser();
@@ -43,7 +48,70 @@ namespace TARSGabrielMoraraRibeiro.Controllers
             }
             catch (Exception e)
             {
-                return Json("Houve um erro interno, já estamos verificando!", HttpStatusCode.InternalServerError);
+                return new JsonResult("Houve um erro interno, já estamos verificando.")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+        }
+
+        public JsonResult UpdateProduct(string productID, string data)
+        {
+            try
+            {
+                if (productID != null && data != null)
+                {
+                    var product = rep._context.Products.FirstOrDefault(s => s.ProductID == Convert.ToInt64(productID));
+                    var newProduct = JsonConvert.DeserializeObject<Products>(data);
+
+                    product.Title = newProduct.Title;
+                    product.Brand = newProduct.Brand;
+                    product.Description = newProduct.Description;
+                    product.Stock = newProduct.Stock;
+                    product.PriceDefault = newProduct.PriceDefault;
+                    product.PriceOffer = newProduct.PriceOffer;
+                    product.ean = newProduct.ean;
+                    product.ncm = newProduct.ncm;
+                    product.Warranty = newProduct.Warranty;
+                    product.Height = newProduct.Height;
+                    product.Lenght = newProduct.Lenght;
+                    product.Width = newProduct.Width;
+                    product.Weight = newProduct.Weight;
+                    product.CategoryID = newProduct.CategoryID;
+                    product.Updated_at = Common.Commons.HourBrasilia;
+
+                    rep._context.SaveChanges();
+
+                    return Json("OK");
+                }
+
+                return new JsonResult("Requisição mal formada.")
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+            catch (Exception e)
+            {
+                return new JsonResult("Houve um erro interno, já estamos verificando.")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+        }
+
+        public JsonResult GetProductByID(string productID)
+        {
+            try
+            {
+                var product = rep._context.Products.FirstOrDefault(s => s.ProductID == Convert.ToInt64(productID));
+                return Json(product);
+            }
+            catch (Exception e)
+            {
+                return new JsonResult("Houve um erro interno, já estamos verificando.")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
             }
         }
 
